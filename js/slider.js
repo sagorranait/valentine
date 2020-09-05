f5b90d9604d51042d13a8a5f463739a4647d7260
SAGOR RANA <60606778+sagorranait@users.noreply.github.com>
2020-09-05 07:49:32 +0600

    Add files via upload

---
+++ js/slider.js
@@ -0,0 +1,21 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    dots:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:2
+        },
+        1200:{
+            items:2
+        }
+    }
+})
+
